f0d322ee9eafadc108d73604a85fa3144e1ee8a4
2504824765 <2504824765@qq.com>
2025-06-22 19:20:30 +0800

    initial commit

---
+++ src/main/java/com/bnwzy/smartclassesspringbootweb/SmartClassesSpringbootWebApplication.java
@@ -0,0 +1,13 @@
+package com.bnwzy.smartclassesspringbootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartClassesSpringbootWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartClassesSpringbootWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bnwzy/smartclassesspringbootweb/SmartClassesSpringbootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnwzy.smartclassesspringbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartClassesSpringbootWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
